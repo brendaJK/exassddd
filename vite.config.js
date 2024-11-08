2f0fc16adf9eb6858286edf3dc6bd48e169fa452
es.bren.27.gtz.p <es.bren.27.gtz.p@gmail.com>
2024-11-08 16:49:51 +0000

    qqqqqqqqqq

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugin: [react(), VitePWA({
-    registerType : 'autoUpdate'
-  }) ],
-  
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    }),
+  ],
+});
