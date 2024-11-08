530b067776963d1f00b75bef991892d8fddfe592
es.bren.27.gtz.p <es.bren.27.gtz.p@gmail.com>
2024-11-08 15:34:52 +0000

    ssss

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
+import { register } from 'module'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugin: [react(), VitePWA({
+    registerType : 'autoUpdate'
+  }) ],
   plugins: [
     react(),
     VitePWA({
